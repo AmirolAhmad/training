ba989fb849cf9f5c6e4c4eba7ea5c0612051f4f8
amirolahmad <9w2pdf@gmail.com>
2015-06-04 15:24:20 +0800

    setup avatar using gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def body_class
   "#{controller_name} #{action_name}"
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "https://gravatar.com/avatar/#{gravatar_id}.png?s=35"
+  end
 end
