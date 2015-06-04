5624fabd2cf4dd5222f18f227b75b60075b92f21
amirolahmad <9w2pdf@gmail.com>
2015-06-04 18:53:26 +0800

    setup the enum booking status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,18 @@
 class Booking < ActiveRecord::Base
+  enum status: {"Active" => 0, "Closed" => 1, "Expired" => 2}
+
   belongs_to :user
   belongs_to :item
+
+  def is_active?
+    status === "Active"
+  end
+
+  def is_closed?
+    status === "Closed"
+  end
+
+  def is_expired?
+    status === "Expired"
+  end
 end
