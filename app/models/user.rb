606c46f0364b55b6121b79dea0cbe4fb2bf8edf9
amirolahmad <9w2pdf@gmail.com>
2015-06-04 18:37:50 +0800

    add status to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,12 +20,13 @@
 
 class User < ActiveRecord::Base
   enum role: [:user, :admin, :supplier]
+  enum status: [:whitelist, :blacklist]
   after_initialize :set_default_role, :if => :new_record?
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   def set_default_role
     self.role ||= :user
+    self.status ||= :whitelist
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150604073921) do
+ActiveRecord::Schema.define(version: 20150604102555) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150604073921) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status",                 limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
