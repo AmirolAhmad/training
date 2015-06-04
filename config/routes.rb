37a1eb27dbc161932cfbb689596119ff223b6678
amirolahmad <9w2pdf@gmail.com>
2015-06-04 17:52:33 +0800

    exclude the item show page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#index'
 
-  resources :items
+  resources :items, except: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
